569ad73ecdad5500b21adddfd0f59331011187a0
Thiago Airton Demeterko <thiago.demeterko@gmail.com>
2022-03-09 23:24:12 -0300

    Config Server com Spring Cloud

---
+++ config-server/src/test/java/br/com/caelum/config/ConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.caelum.config;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
