8f4e8fa3e2f54c54c32b0ed728fec783d127837f
Thiago Airton Demeterko <thiago.demeterko@gmail.com>
2022-03-09 21:11:57 -0300

    Distributed Tracing com Sleuth e Papertrail

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/ClienteRestDoPedido.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/ClienteRestDoPedido.java
@@ -2,6 +2,8 @@ package br.com.caelum.eats.pagamento;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
@@ -14,16 +16,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Service
 public class ClienteRestDoPedido {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ClienteRestDoPedido.class);
+
     @Autowired
     private PedidoClient pedidoClient;
 
     void notificaPagamentoDoPedido(Long pedidoId) {
 
+        LOG.info("Notificando pagamento do pedido {}", pedidoId);
         ResponseEntity<?> response = pedidoClient.atualizaStatus(pedidoId, new PedidoMudancaDeStatusRequest("pago".toUpperCase()));
 
         if (!HttpStatus.valueOf(response.getStatusCodeValue()).is2xxSuccessful()) {
+            LOG.info("problema ao tentar mudar o status do pedido: " + pedidoId);
             throw new RuntimeException("problema ao tentar mudar o status do pedido: " + pedidoId);
         }
+
+        LOG.info("Notificado com sucesso o pagamento do pedido {}", pedidoId);
     }
 }
 
