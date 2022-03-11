52e0dcade9144d91e558f6daac3530df0b2a6489
Thiago Airton Demeterko <thiago.demeterko@gmail.com>
2022-03-11 00:50:32 -0300

    Load Balancer com OpenFeign no monolito

--- eats-monolito/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/RestauranteController.java
+++ eats-monolito/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/RestauranteController.java
@@ -1,26 +1,23 @@
 package br.com.caelum.eats.restaurante;
 
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
 class RestauranteController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(RestauranteController.class);
+
 	private RestauranteRepository restauranteRepo;
 	private CardapioRepository cardapioRepo;
+	private DistanciaClient distanciaClient;
 
 	@GetMapping("/restaurantes/{id}")
 	RestauranteDto detalha(@PathVariable("id") Long id) {
@@ -46,15 +43,25 @@ class RestauranteController {
 		Cardapio cardapio = new Cardapio();
 		cardapio.setRestaurante(restauranteSalvo);
 		cardapioRepo.save(cardapio);
+
+		LOG.info("Incluindo restaurante {} no serviço de distância", restaurante.getNome());
+		distanciaClient.adiciona(new RestauranteDistanciaDto(restauranteSalvo.getId(), restauranteSalvo.getCep(), restauranteSalvo.getTipoDeCozinha().getId()));
+		LOG.info("Incluído restaurante {} no serviço de distância", restaurante.getNome());
+
 		return restauranteSalvo;
 	}
 
-  @PutMapping("/parceiros/restaurantes/{id}")
-  public RestauranteDto atualiza(@PathVariable Long id, @RequestBody RestauranteDto restaurante) {
-    Restaurante doBD = restauranteRepo.getOne(id);
-    restaurante.populaRestaurante(doBD);
-    return new RestauranteDto(restauranteRepo.save(doBD));
-  }
+	@PutMapping("/parceiros/restaurantes/{id}")
+	public RestauranteDto atualiza(@PathVariable("id") Long id, @RequestBody RestauranteDto restaurante) {
+		Restaurante doBD = restauranteRepo.getOne(id);
+		restaurante.populaRestaurante(doBD);
+
+		LOG.info("Atualizando serviço de distância para o restaurante {}", restaurante.getNome());
+		distanciaClient.atualiza(doBD.getId(), new RestauranteDistanciaDto(doBD.getId(), restaurante.getCep(), restaurante.getTipoDeCozinha().getId()));
+		LOG.info("Atualizado serviço de distância para o restaurante {}", restaurante.getNome());
+
+		return new RestauranteDto(restauranteRepo.save(doBD));
+	}
 
 
   @GetMapping("/admin/restaurantes/em-aprovacao")
